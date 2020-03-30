706b8ff2bc51910447fa08695c4aa2ba3b090a22
misterchaos <kobe524348@gmail.com>
2020-03-30 16:07:50 +0800

    feat(constant):添加命令行指令参数常量

--- src/com/gdut/generator/constant/Constant.java
+++ src/com/gdut/generator/constant/Constant.java
@@ -6,4 +6,27 @@ package com.gdut.generator.constant;
  * @date 2020-03-27 16:15
  */
 public class Constant {
+
+
+
+    /***********************************************************
+     *                  命令行参数常量
+     ***********************************************************/
+
+    /**
+     * 用于指定题目中数值（自然数、真分数和真分数分母）的范围
+     */
+    public static final String OPS_R = "-r";
+    /**
+     * 用于指定生成题目的个数
+     */
+    public static final String OPS_N = "-n";
+    /**
+     * 用于指定输出题目的文件路径
+     */
+    public static final String OPS_E = "-e";
+    /**
+     * 用于指定输出答案的文件路径
+     */
+    public static final String OPS_A = "-a";
 }
